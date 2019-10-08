6e5a99d27ec2b217c7ba7e17a21137190a1adc99
Pensivej <jshim10@illinois.edu>
2019-10-09 00:29:41 +0900

    아래 textarea는 자동으로 2줄에서 6줄까지 늘어나게 구현, commentLimit 이라는 항목을 줘서 true면 경고메시지 출력, 버튼 두개 생성

--- app/src/Components/Summary.js
+++ app/src/Components/Summary.js
@@ -1,38 +1,76 @@
 import React, { Component } from 'react';
-import { Row, Col, Typography } from 'antd';
+import { Row, Col, Typography, Input, Button } from 'antd';
 
 const { Title, Paragraph } = Typography;
+const { TextArea } = Input;
 
 export default class Summary extends Component {
     state = {
-        
+        endTime: (new Date(Date.now())).toLocaleString(),
+        comment: null,
+        commentLimit: false,
     };
 
+    onCommentChange = ({ target: { value } }) => {
+        // TODO: 140자 제한 두고, 제한 넘으면 commentLimit -> true
+        this.setState({
+            comment: value,
+        })
+    }
+
     render() {
+        const { endTime, comment, commentLimit } = this.state;
+
         return (
-            <Row style={{width: '60%', minWidth: '320px', marginLeft: '2vw'}}>
+            <Row gutter={8} style={{ width: '60%', minWidth: '320px', margin: 'auto', left: '-24px' }}>
                 <Col>
-                    <Paragraph strong style={{textAlign:'right', fontSize:'0.8em'}}>4000 ETH</Paragraph>
+                    <Paragraph strong style={{ textAlign: 'right', fontSize: '0.8em' }}>4000 ETH</Paragraph>
                 </Col>
                 <Col span={6}>
-                    <Paragraph strong style={{textAlign:'right', fontSize:'0.8em'}}>- Gas Fee</Paragraph>
+                    <Paragraph strong style={{ textAlign: 'right', fontSize: '0.8em' }}>- Gas Fee</Paragraph>
                 </Col>
                 <Col span={18}>
-                    <Paragraph strong style={{textAlign:'right', fontSize:'0.8em'}}>2 ETH</Paragraph>
+                    <Paragraph strong style={{ textAlign: 'right', fontSize: '0.8em' }}>2 ETH</Paragraph>
                 </Col>
-                <Col span={24} style={{width: '110%'}}>
-                    <hr/>
+                <Col span={24} style={{ width: '110%' }}>
+                    <hr />
                 </Col>
                 <Col span={6}>
-                    <Paragraph strong style={{textAlign:'right', fontSize:'0.8em'}}>Deposit</Paragraph>
+                    <Paragraph strong style={{ textAlign: 'right', fontSize: '0.8em' }}>Deposit</Paragraph>
                 </Col>
                 <Col span={18}>
-                    <Paragraph strong style={{textAlign:'right', fontSize:'0.8em'}}>3988 ETH</Paragraph>
+                    <Paragraph strong style={{ textAlign: 'right', fontSize: '0.8em' }}>3988 ETH</Paragraph>
                 </Col>
                 <Col>
-                    <Title strong level={1} style={{textAlign: 'center', marginBottom: '0.1em'}}>3,998 ETH</Title>
-                    <Paragraph strong style={{textAlign: 'center'}}>will be tied up until</Paragraph>
-                    <Title strong level={3} style={{textAlign: 'center', marginTop: '0'}}>{(new Date(Date.now())).toLocaleString()}</Title>
+                    <Title strong level={1} style={{ textAlign: 'center', marginBottom: '0.1em' }}>3,998 ETH</Title>
+                    <Paragraph strong style={{ textAlign: 'center' }}>will be tied up until</Paragraph>
+                    <Title strong level={3} style={{ textAlign: 'center', marginTop: '0' }}>{endTime}</Title>
+                </Col>
+                <Col>
+                    <TextArea
+                        value={comment}
+                        onChange={this.onCommentChange}
+                        placeholder={'Add a comment'}
+                        autosize={{ minRows: 2, maxRows: 6 }}
+                    />
+                    {
+                        commentLimit ?
+                            <Typography
+                                style={{ fontSize: '0.8em', color: 'red' }}>
+                                Comment cannot exceed 140 characters
+                                </Typography>
+                            : null
+                    }
+                </Col>
+                <Col span={12}>
+                    <Button type="default" block style={{ marginTop: '1em' }}>
+                        Go Back
+                    </Button>
+                </Col>
+                <Col span={12}>
+                    <Button type="primary" block style={{ marginTop: '1em' }}>
+                        Confirm
+                    </Button>
                 </Col>
             </Row>
         );
