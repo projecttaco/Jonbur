201aa0d5dce2fc178e653389a39fb5e368ed476b
Pensivej <jshim10@illinois.edu>
2019-10-21 22:28:36 +0900

    deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
 const TutorialToken = artifacts.require("TutorialToken");
 const ComplexStorage = artifacts.require("ComplexStorage");
+const Jonbur = artifacts.require("Jonbur");
 
 module.exports = function(deployer) {
+  deployer.deploy(Jonbur);
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
