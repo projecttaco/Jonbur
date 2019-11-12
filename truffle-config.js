68ff948f6cdc431d4300779d5d1b0976c553a7ec
cloudgray <bluejet1104@gmail.com>
2019-11-12 20:24:31 +0900

    deploy to rinkeby and ropsten testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
-const path = require("path");
 require("dotenv").config();
+const path = require("path");
 
 /**
  * This truffle script will deploy your smart contracts to your SKALE Chain.
@@ -17,16 +17,8 @@ let privateKey = process.env.YOUR_PRIVATE_KEY;
 // Provide your SKALE endpoint address
 let skale = process.env.YOUR_SKALE_CHAIN_ENDPOINT_1;
 
-/**
- * This truffle script will deploy your smart contracts to ropsten
- * ethereum testnet using infura service
- *
- * @param {string} testMnemonic - Provide your wallet mnemonic.
- * @param {string} infuraKey - Provide your infura key.
- */
-
+// Provider your wallet mnemonic to use infura service
 let testMnemonic = process.env.TEST_MNEMONIC;
-let infuraKey = process.env.INFURA_KEY;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -45,12 +37,23 @@ module.exports = {
       provider: () =>
         new HDWalletProvider(
           testMnemonic,
-          "https://ropsten.infura.io/v3/" + infuraKey,
-          0
+          //  디버깅이 완료되면 추후에 수정 예정
+          "https://ropsten.infura.io/v3/" + "d060c760797b4ab3bc3ed99f9e82d626"
         ),
       network_id: 3,
       gas: 4700000,
-      gasPrice: 1000000000000
+      gasPrice: 10000000000
+    },
+    "rinkeby-infura": {
+      provider: () =>
+        new HDWalletProvider(
+          testMnemonic,
+          //  디버깅이 완료되면 추후에 수정 예정
+          "https://rinkeby.infura.io/v3/" + "d060c760797b4ab3bc3ed99f9e82d626"
+        ),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000
     }
   }
 };
