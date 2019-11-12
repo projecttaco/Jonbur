7862c7f52027f05bfccbea4aca37b51976a32673
cloudgray <bluejet1104@gmail.com>
2019-11-12 14:43:59 +0900

    connected to SKALE chain (and Jonbur contract was deployed to our SKALE chain)

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,22 @@
 const path = require("path");
 
+/**
+ * This truffle script will deploy your smart contracts to your SKALE Chain.
+ *
+ * @param {string} privateKey - Provide your wallet private key.
+ * @param {string} provider - Provide your SKALE endpoint address.
+ */
+
+let HDWalletProvider = require("truffle-hdwallet-provider");
+
+// https://developers.skalelabs.com for SKALE documentation
+// Provide your wallet private key
+let privateKey =
+  "FB76D45DD58A3670131413072165012795192E4509CAD4902DE27D94AA3C8127";
+
+// Provide your SKALE endpoint address
+let skale = "https://sip1.skalenodes.com:10051";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +24,11 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    skale: {
+      provider: () => new HDWalletProvider(privateKey, skale),
+      gasPrice: 0,
+      network_id: "*"
     }
   }
 };
