82830de4dd953a2069396a15ba0ae8e12dcbfaa1
Pensivej <jshim10@illinois.edu>
2019-11-19 22:21:34 +0900

    불필요한 파일들 삭제

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,5 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
 const Jonbur = artifacts.require("Jonbur");
 
 module.exports = function(deployer) {
   deployer.deploy(Jonbur);
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
 };
