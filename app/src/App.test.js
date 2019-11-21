0f8a415e345fb333d4f74f4752f607d894b2670c
Pensivej <jshim10@illinois.edu>
2019-11-21 16:02:24 +0900

    test file

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import options from "./drizzleOptions";
+import { DrizzleProvider } from 'drizzle-react';
+import LoadingContainer from "./Components/LoadingContainer";
+import store from "./store";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<DrizzleProvider options={options} store={store}>
+    <LoadingContainer>
+        <App />
+    </LoadingContainer>
+</DrizzleProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
