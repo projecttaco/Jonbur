1489384857db469d2a97e7e49b32bb879a37d70d
zustkeeper <zustkeeper@naver.com>
2019-12-02 02:37:44 +0900

    Picker에 defalutValue 현재시간으로 설정, onChange 저장하도록

--- app/src/Components/DateInput.js
+++ app/src/Components/DateInput.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
 import { Row, Col, DatePicker, TimePicker } from 'antd';
+import { drizzleConnect } from "drizzle-react";
+// import moment from 'moment';
 
-export default class DateInput extends Component {
-    state = {
-        
-    };
+
+class DateInput extends Component {
+    constructor(props, context) {
+        super(props);
+
+        this.state = {
+            
+        };
+    }
+
+    onChange(date, dateString) {
+        console.log(date, dateString);
+      }
 
     render() {
+        const { inputDate, inputTime } = this.props;
+        // const { year, month, day, hour, minute, second } = this.props;
+        // console.log(withdrawDate);
         return (
             <Row justify={'space-between'}>
                 <Col span={14}>
-                    <DatePicker style={{ width: 280 }} />
+                    <DatePicker style={{ width: 280 }} defaultValue={inputDate} onChange={e => this.props.dateChange(e)}/>
                 </Col>
                 <Col span={8}>
-                    <TimePicker style={{ width: 168 }} />
+                    <TimePicker style={{ width: 168 }} defaultValue={inputTime} onChange={e => this.props.timeChange(e)}/>
                 </Col>
             </Row>
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        state: state,
+        account: state.accounts[0],
+        balance: state.accountBalances[state.accounts[0]],
+        inputDate: state.deposit.date,
+        inputTime: state.deposit.time,
+        // year : state.deposit.year,
+        // month : state.deposit.month,
+        // day : state.deposit.day,
+        // hour : state.deposit.hour,
+        // minute : state.deposit.minute,
+        // second : state.deposit.second,
+        // withdrawDate: state.deposit.withdrawDate,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        dateChange: date => dispatch({type: 'UPDATE_DATE', date: date}),
+        timeChange: time => dispatch({type: 'UPDATE_TIME', time: time}),
+    };
+}
+
+export default drizzleConnect(DateInput, mapStateToProps, mapDispatchToProps);
