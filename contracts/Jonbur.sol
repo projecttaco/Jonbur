7d83a5730a327dc466db15fdfd3d296e63f0914e
zustkeeper <52707622+zustkeeper@users.noreply.github.com>
2019-12-02 16:03:57 +0900

    Jonbur.sol v2

--- contracts/Jonbur.sol
+++ contracts/Jonbur.sol
@@ -5,39 +5,59 @@ contract Jonbur{
         uint date;
         string comment;
         uint amount;
+        bool spent;
+        address hodlOwner;
     }
     
     address public owner;
-    mapping(address => Hodl) hodlers;
+    uint public hodlIndex;
+    mapping(uint => Hodl) hodls;
+    mapping(address => uint[]) hodlers;
     
     event HodlerAdded(address _addr, string _comment);
     event HodlerRewarded(address _addr);
     
     constructor() public{
         owner = msg.sender;
-    }
-
-
-    function getComment(address _addr) view public returns (string memory) {
-        return hodlers[_addr].comment;
+        hodlIndex = 0;
     }
     
-    function getMyComment() view public returns (string memory) {
-        return hodlers[msg.sender].comment;
+    function getHodlIndexes() view private returns (uint[] memory){
+        return hodlers[msg.sender];
+    }
+    
+    function getHodls() view public returns (uint[] memory, uint[] memory, uint[] memory, bool[] memory){
+        uint[] memory indexes = getHodlIndexes();
+        uint[] memory hodlDate = new uint[](indexes.length);
+        uint[] memory hodlAmount = new uint[](indexes.length);
+        bool[] memory hodlSpent = new bool[](indexes.length);
+        Hodl memory tempHodl;
+        for(uint i = 0; i < indexes.length; i++){
+            tempHodl = hodls[indexes[i]];
+            hodlDate[i] = tempHodl.date;
+            hodlAmount[i] = tempHodl.amount;
+            hodlSpent[i] = tempHodl.spent;
+        }
+        return (indexes, hodlDate, hodlAmount, hodlSpent);
+    }
+    
+    function getComment(uint _index) view public returns (string memory){
+        return hodls[_index].comment;
     }
     
     function deposit(uint _date, string memory _comment) public payable {
-        require(hodlers[msg.sender].amount == 0, "Already has a Jonbur");
-        hodlers[msg.sender] = Hodl(_date, _comment, msg.value);
+        hodlers[msg.sender].push(hodlIndex);
+        hodls[hodlIndex] = Hodl(_date, _comment, msg.value, false, msg.sender);
+        hodlIndex++;
         emit HodlerAdded(msg.sender, _comment);
     }
     
-    function withdraw() public {
-        require(hodlers[msg.sender].date > 0, "Don't have a Jonbur");
-        require(hodlers[msg.sender].date <= block.timestamp, "Not yet");
-        msg.sender.transfer(hodlers[msg.sender].amount);
+    function withdraw(uint _index) public {
+        require(hodls[_index].hodlOwner == msg.sender, "Invalid Jonbur");
+        require(hodls[_index].spent == false, "Already Rewarded");
+        require(hodls[_index].date <= block.timestamp, "Not yet");
+        msg.sender.transfer(hodls[_index].amount);
         emit HodlerRewarded(msg.sender);
-        hodlers[msg.sender].amount = 0;
-        hodlers[msg.sender].comment = "";
+        hodls[_index].spent = true;
     }
 }
