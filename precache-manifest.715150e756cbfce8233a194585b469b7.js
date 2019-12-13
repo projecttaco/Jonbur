14a3fcfbb7d0814c59af516b95abc78ad4632ec6
Pensivej <jshim10@illinois.edu>
2019-12-14 03:10:48 +0900

    Updates

---
+++ precache-manifest.715150e756cbfce8233a194585b469b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ea617082c03a7f2aaa8",
+    "url": "/jonbur/static/css/main.96fbae54.chunk.css"
+  },
+  {
+    "revision": "2ea617082c03a7f2aaa8",
+    "url": "/jonbur/static/js/main.2ea61708.chunk.js"
+  },
+  {
+    "revision": "bd116b44a388e12f505a",
+    "url": "/jonbur/static/css/1.2a5d076c.chunk.css"
+  },
+  {
+    "revision": "bd116b44a388e12f505a",
+    "url": "/jonbur/static/js/1.bd116b44.chunk.js"
+  },
+  {
+    "revision": "2662103322b4121b8045",
+    "url": "/jonbur/static/js/runtime~main.26621033.js"
+  },
+  {
+    "revision": "5e06983f848d06d5a7a2494be6174082",
+    "url": "/jonbur/static/media/metamask.5e06983f.png"
+  },
+  {
+    "revision": "387c5d83dc7c034cd6e2252dcadedb93",
+    "url": "/jonbur/static/media/opera.387c5d83.svg"
+  },
+  {
+    "revision": "6da1f0b0710b88713ffc069b2e1bc038",
+    "url": "/jonbur/index.html"
+  }
+];
