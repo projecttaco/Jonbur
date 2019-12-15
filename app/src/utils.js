c19d8c2a18d9c0fa43aec0a8a0d97d78cb182599
Pensivej <jshim10@illinois.edu>
2019-12-15 23:09:43 +0900

    필요없는 함수 제거

--- app/src/utils.js
+++ app/src/utils.js
@@ -1,33 +1,7 @@
-import axios from "axios";
-
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
   minimumFractionDigits: 2
 });
 
-const getUSD = () => {
-  try {
-    return axios.get(
-      "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD"
-    );
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const getPrice = () => {
-  // getUSD()
-  //   .then(res => {
-  //     let usd = res.data.USD;
-  //     if (usd) {
-  //       console.log(`eth_usd: ${usd}`);
-  //       return usd;
-  //     }
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
-};
-
-export { formatter, getPrice };
+export { formatter };
